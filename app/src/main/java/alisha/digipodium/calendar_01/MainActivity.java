c41334fe511ddedc5ecc1882a620fcfc2baf88d9
Alisha Singh <63709954+Alisha-creator@users.noreply.github.com>
2020-11-28 22:27:56 +0530

    Initial commit

---
+++ app/src/main/java/alisha/digipodium/calendar_01/MainActivity.java
@@ -0,0 +1,30 @@
+package alisha.digipodium.calendar_01;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView ;
+    TextView myDate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView =(CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (month + 1 ) + "-" + dayOfMonth + "-" + month;
+                myDate.setText(date);
+            }
+        });
+    }
+}
